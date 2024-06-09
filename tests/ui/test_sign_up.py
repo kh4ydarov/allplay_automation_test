c9e91f52c9c05e2e6a221daae3db4d21426700e6
kh4ydarov <kh4ydarovs@yandex.ru>
2024-06-09 12:04:51 +0500

    hw

--- tests/ui/test_sign_up.py
+++ tests/ui/test_sign_up.py
@@ -2,7 +2,7 @@ from allplay_tests.pages.open_page import open_page
 from allplay_tests.pages.authorization import authorization
 
 
-def test_auth_page():
+def test_sign_up():
     open_page.open_site()
     open_page.authorization_page()
     open_page.asserting_auth_page()
