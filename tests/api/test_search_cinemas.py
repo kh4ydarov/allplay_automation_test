011ca5d61c298791cada74987778923a5d58e792
kh4ydarov <kh4ydarovs@yandex.ru>
2024-06-15 17:38:20 +0500

    Allplay autotests

--- tests/api/test_search_cinemas.py
+++ tests/api/test_search_cinemas.py
@@ -15,13 +15,13 @@ from allplay_tests.schemas.search_cinema import search_movie
 def test_search(base_api_url):
     with allure.step('Send request with valid data'):
         response = requests.get(f'{base_api_url}/api/v1/movies',
-                                data={"search": "Наруто", "per_page": "1"})
+                                data={"search": "Не с первой попытки", "per_page": "1"})
 
     with allure.step('Status code=200'):
         assert response.status_code == 200
 
         response_json = response.json()
     with allure.step('Checking whether the movie name matches the request'):
-        assert response_json['data'][0]['title'] == 'Наруто'
+        assert response_json['data'][0]['title'] == 'Не с первой попытки'
     with allure.step('Schema is validate'):
         validate(response.json(), search_movie)
