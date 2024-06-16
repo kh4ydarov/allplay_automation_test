e4a048e8552a4eafb7cefa690316fd5527473d29
kh4ydarov <kh4ydarovs@yandex.ru>
2024-06-16 09:33:21 +0500

    Allplay autotests

--- allplay_tests/pages/ui/authorization_page.py
+++ allplay_tests/pages/ui/authorization_page.py
@@ -10,12 +10,6 @@ class Authorization:
         with allure.step("Регистрация нового пользователя"):
             browser.element('.Login__link').click()
             browser.element('#referralId').perform(command.js.scroll_into_view).click()
-            # browser.element('#name').set_value(user.name)
-            # browser.element('#email').set_value(user.email)
-            # browser.element('#password').set_value(user.password)
-            # browser.element('#confirmPassword').set_value(user.confirm_password)
-            # browser.element('#remember').click()
-            # browser.element('.Login__submit').click()
         return self
 
     def asserting_sign_up_page(self):
